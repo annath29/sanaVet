0e20b913b77541b1c9d60b31d1cfcfec8ec614eb
guerreroaanathalia@gmail.com <guerreroaanathalia@gmail.com>
2023-09-01 15:09:13 -0500

    inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListAppointmentsComponent } from './components/appointment/list/list-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListAppointmentsComponent
   ],
   imports: [
     BrowserModule
